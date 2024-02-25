bf1ade2eacef882dc3d2a898923d2392d248387c
Paul Robson <smawl1@hotmail.com>
2024-02-25 21:31:32 +0000

    Added more basic item UpdateQualityTests

--- csharpcore-Verify.xunit/GildedRoseTests/ItemTests.cs
+++ csharpcore-Verify.xunit/GildedRoseTests/ItemTests.cs
@@ -13,7 +13,34 @@ namespace GildedRoseTests
         [InlineData(0, 0)]
         public void BasicItem_UpdateQualityOnce_QualityIsCorrectValue(int startingQuality, int expectedQuality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = startingQuality } };
+            IList<Item> Items = new List<Item> { 
+                new Item { 
+                    Name = "foo", 
+                    SellIn = 10, 
+                    Quality = startingQuality 
+                } 
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(expectedQuality, Items[0].Quality);
+        }
+
+        [Theory]
+        [InlineData(50, 48)]
+        [InlineData(25, 23)]
+        [InlineData(3, 1)]
+        [InlineData(2, 0)]
+        [InlineData(1, 0)]
+        [InlineData(0, 0)]
+        public void BasicItemWithSellByDatePassed_UpdateQualityOnce_QualityIsCorrectValue(int startingQuality, int expectedQuality)
+        {
+            IList<Item> Items = new List<Item> {
+                new Item {
+                    Name = "foo",
+                    SellIn = 0,
+                    Quality = startingQuality
+                }
+            };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.Equal(expectedQuality, Items[0].Quality);
