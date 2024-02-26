59ce42b281c49282b5899283a78f65c8462659f1
Paul Robson <smawl1@hotmail.com>
2024-02-26 21:31:43 +0000

    Fixed EnsureValidValues lack of max value + proper constants for min and max

--- csharpcore-Verify.xunit/GildedRose/Extensions/ItemExtension.cs
+++ csharpcore-Verify.xunit/GildedRose/Extensions/ItemExtension.cs
@@ -2,13 +2,20 @@
 {
     public static class ItemExtension
     {
-        public static void EnsureNoNegativeQuality(this Item item)
+        private const int MIN_QUALITY_VALUE = 0;
+        private const int MAX_QUALITY_VALUE = 50;
+
+        public static void EnsureValidValues(this Item item)
         {
             if (item != null)
             {
                 if (item.Quality < 0)
                 {
-                    item.Quality = 0;
+                    item.Quality = MIN_QUALITY_VALUE;
+                }
+                if (item.Quality > MAX_QUALITY_VALUE)
+                {
+                    item.Quality = MAX_QUALITY_VALUE;
                 }
             }
         }
